7dcfd54ef2d2c28c856da47f9343cb13d5b80b8b
Deniz Sert <dsert@mit.edu>
2022-12-22 16:06:46 -0800

    send detailed json for debugging

--- middleware/validate-line-signature.js
+++ middleware/validate-line-signature.js
@@ -8,7 +8,9 @@ const validateLineSignature = (req, res, next) => {
   const { LINE_CHANNEL_SECRET } = config;
   const signature = req.header('x-line-signature');
   if (LINE_CHANNEL_SECRET && !validateSignature(rawBody, LINE_CHANNEL_SECRET, signature)) {
-    res.sendStatus(403);
+    // res.sendStatus(403);
+    res.status(403).json({ "line_channel_secret": LINE_CHANNEL_SECRET,
+                            "!validateSignature(rawBody, LINE_CHANNEL_SECRET, signature)": !validateSignature(rawBody, LINE_CHANNEL_SECRET, signature) });
     return;
   }
   next();
