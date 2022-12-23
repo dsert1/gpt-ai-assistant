c131983b46e87232a3fcd34b05c04f80dd004dec
Deniz Sert <dsert@mit.edu>
2022-12-22 17:10:56 -0800

    added console logs for validate line sig

--- middleware/validate-line-signature.js
+++ middleware/validate-line-signature.js
@@ -5,6 +5,7 @@ import {
 
 const validateLineSignature = (req, res, next) => {
   const { rawBody } = req;
+  console.log(rawBody);
   const { LINE_CHANNEL_SECRET } = config;
   const signature = req.header('x-line-signature');
   if (LINE_CHANNEL_SECRET && !validateSignature(rawBody, LINE_CHANNEL_SECRET, signature)) {
